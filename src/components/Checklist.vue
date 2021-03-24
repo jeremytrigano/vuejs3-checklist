88b60ba2c3d11568eecaf7833170077d2d72e57b
Jérémy Trigano <52828091+jeremytrigano@users.noreply.github.com>
2021-03-24 10:20:12 +0100

    edit method

--- src/components/Checklist.vue
+++ src/components/Checklist.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="checklist">
     <h1>{{ msg }}</h1>
-    <input type="text" v-model="newTodo" /><button
-      @click="addTodo"
-      v-if="newTodo != ''"
-    >
-      Ajouter
-    </button>
+    Ajouter un nouveauToDo :
+    <input
+      class="mb-1"
+      type="text"
+      v-model="newTodo"
+      @keypress.enter="addTodo"
+    />&nbsp;
+    <div class="buttonDiv">
+      <button
+        class="action-button shadow animate blue"
+        @click="addTodo"
+        v-if="newTodo != ''"
+      >
+        Ajouter
+      </button>
+    </div>
     <br />
-    <br />
-    <Todo :todos="todos" @delete-todo="deleteTodo" />
+    <Todo :todos="todos" @delete-todo="deleteTodo" @edit-todo="editTodo" />
   </div>
 </template>
 
@@ -37,6 +46,10 @@ export default {
       newTodo.value = '';
     };
 
+    const editTodo = (todo) => {
+      todos.value = todos.value.map((t) => (t.id !== todo.id ? t : todo));
+    };
+
     const deleteTodo = (todo) => {
       todos.value = todos.value.filter((t) => t.id !== todo.id);
     };
@@ -44,6 +57,7 @@ export default {
     return {
       msg: 'Bienvenue sur votre Checklist',
       addTodo,
+      editTodo,
       deleteTodo,
       todos,
       newTodo,
@@ -59,10 +73,59 @@ export default {
   text-align: center;
 }
 
+.buttonDiv {
+  height: 40px;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
   margin: 0;
   text-align: left;
+}
+
+.mb-1 {
+  margin-bottom: 1em;
+}
+
+::v-deep .animate {
+  transition: all 0.1s;
+  -webkit-transition: all 0.1s;
+}
+
+::v-deep .action-button {
+  position: relative;
+  padding: 1px 7px;
+  margin: 0px 10px 10px 0px;
+  border-radius: 5px;
+  font-family: 'Pacifico', cursive;
+  color: #fff;
+  text-decoration: none;
+  border-style: none;
+  outline-style: none;
+}
+
+::v-deep .blue {
+  background-color: #3498db;
+  border-bottom: 5px solid #2980b9;
+  text-shadow: 0px -2px #2980b9;
+}
+
+::v-deep .red {
+  background-color: #e74c3c;
+  border-bottom: 5px solid #bd3e31;
+  text-shadow: 0px -2px #bd3e31;
+}
+
+::v-deep .green {
+  background-color: #82bf56;
+  border-bottom: 5px solid #669644;
+  text-shadow: 0px -2px #669644;
+}
+
+::v-deep .action-button:active {
+  transform: translate(0px, 5px);
+  -webkit-transform: translate(0px, 5px);
+  border-bottom: 1px solid;
 }
 </style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,15 +1,37 @@
 <template>
   <ul>
     <li v-for="todo in todos" :key="todo.id">
-      <button @click="deleteTodo(todo)">x</button>{{ todo.todo }}
+      <button
+        class="action-button shadow animate red"
+        @click="deleteTodo(todo)"
+      >
+        x
+      </button>
+      <button class="action-button shadow animate blue" @click="editTodo(todo)">
+        Editer
+      </button>
+      &nbsp;<span v-if="todoToEdit !== null && todoToEdit.id === todo.id">
+        <input
+          type="text"
+          v-model="todoToEdit.todo"
+          @keypress.enter="save"
+        />&nbsp;<button
+          class="action-button shadow animate green"
+          @click="save"
+        >
+          Sauvegarder
+        </button> </span
+      ><span v-else>{{ todo.todo }}</span>
     </li>
   </ul>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'Todo',
-  emits: ['delete-todo'],
+  emits: ['delete-todo', 'edit-todo'],
   props: {
     todos: {
       type: Array,
@@ -17,10 +39,21 @@ export default {
     },
   },
   setup(props, { emit }) {
+    let todoToEdit = ref(null);
+
     const deleteTodo = (todo) => {
       emit('delete-todo', todo);
     };
-    return { deleteTodo };
+    const editTodo = (todo) => {
+      todoToEdit.value = todo;
+    };
+    const save = () => {
+      emit('edit-todo', todoToEdit.value);
+      todoToEdit.value = null;
+    };
+    return { deleteTodo, editTodo, save, todoToEdit };
   },
 };
 </script>
+
+<style scoped></style>
